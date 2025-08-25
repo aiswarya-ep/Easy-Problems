494bf2ed2660bdb5543225e02cd652b6278ae82e
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-25 21:48:28 +0530

    Add files via upload

---
+++ program8.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i,r;
+    cout << "enter the dividend:\n";
+    cin>>n;
+    cout << "enter the divisor:\n";
+    cin>>i;
+    r=n%i;
+    cout << "\nremainder is:"<<r;
+    return 0;
+}
