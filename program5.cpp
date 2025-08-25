2e759ff0f0a79da077247ec3c8cebf47445eb28e
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-25 21:32:35 +0530

    Add files via upload

---
+++ program5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int c,f;
+    cout<<"enter degree in celsius:";
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<<"\ndegree in fahrenheit is : ";
+    cout<<f;
+    return 0;
+}
