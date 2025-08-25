5ef4ce3429ab331c336e004fb348cb60c3b9f366
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-25 21:21:13 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,sum;
+    cout << "enter two numbers:\n";
+    cout<<"number 1:";
+    cin>>x;
+    cout<<"number 2:";
+    cin>>y;
+    sum=x+y;
+    cout<<"sum is:"<<sum;
+    return 0;
+}
