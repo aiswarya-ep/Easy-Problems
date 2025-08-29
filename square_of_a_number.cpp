2e5f566bd7e0fb3dc6b19664faf5c4d38b39ca9b
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-29 21:42:09 +0530

    Add files via upload

---
+++ square_of_a_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,sq;
+    cout << "enter a number: \n";
+    cin>>x;
+    sq=x*x;
+    cout<<"\nsqaure of the number is"<<sq;
+    return 0;
+}
