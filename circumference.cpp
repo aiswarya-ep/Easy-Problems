46c4a42b614d1cb32dfbf9a8c42c0b5b08cde4ee
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-29 21:56:57 +0530

    Add files via upload

---
+++ circumference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    float circ;
+    cout << "enter radius: ";
+    cin>>r;
+    circ=2*3.14*r;
+    cout << "circumference is: \n"<<circ;
+    return 0;
+}
