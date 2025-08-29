7a469b9221db82e1185db55cade32fb6a7ca93f2
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-29 22:44:06 +0530

    Add files via upload

---
+++ sum_of_natural_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"sum of first 10 natural numbers are:";
+    for(i=1;i<=10;i++)
+    {
+        cout<<i<<"\n";
+    }
+
+    return 0;
+}
