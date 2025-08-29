59ff315610e9d8099404d48a3eae84f66cf11006
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-29 22:35:59 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "enter a character: ";
+    cin>>ch;
+    cout << "ASCII value is : "<<int(ch);
+
+    return 0;
+}
