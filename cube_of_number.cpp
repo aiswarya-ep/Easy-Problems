4c664db6ccb66477aa0a82b2d345b593f985bd0c
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-29 21:43:23 +0530

    Add files via upload

---
+++ cube_of_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,cu;
+    cout << "enter a number: \n";
+    cin>>x;
+    cu=x*x*x;
+    cout<<"\ncube of the number is"<<cu;
+    return 0;
+}
