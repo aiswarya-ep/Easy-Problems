6030402d2ddcd8181164be403eca49fdd5849d71
aiswarya-ep <aiswaryaep07@gmail.com>
2025-08-31 09:47:50 +0530

    Add files via upload

---
+++ vowel-or_consonant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "enter a character:\n";
+    cin>>ch;
+    if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    {
+        cout<<"\nthe character youve entered is a vowel";
+    }
+    else
+    {
+        cout<<"\nthe character youve entered is a consonant";
+    }
+
+    return 0;
+}
