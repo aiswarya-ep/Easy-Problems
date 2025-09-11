7834cd946c20a55cedad03032fe968edae62990f
aiswarya-ep <aiswaryaep07@gmail.com>
2025-09-11 22:20:36 +0530

    even_numbers_till_n.cpp

--- even_numbers_till_n.cpp
+++ even_numbers_till_n.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 int main()
 {
-    int num;
+    int num,sum=0;
     cout << "Enter the limit ";
     cin >> num;
+    cout<<"Even numbers till the given limit are";
     for(int i=1;i<=num;i++)
     {
         if(i%2==0)
         {
-            cout<<i<<endl;
+            cout<< "\n"<<i <<endl;
+            sum+=i;
         }
     }
+    cout<<"sum is:"<<sum;
     return 0;
 }
+
+
